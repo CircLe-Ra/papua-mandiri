5ac9f58836f1c8a31974ccef765a392727695c33
CircLe-Ra <herrylapandera@gmail.com>
2025-07-08 23:57:24 +0000

    up

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
 
     'midtrans' => [
         'serverKey'     => env('MIDTRANS_SERVER_KEY'),
-        'clientKey'     => env('VITE_MIDTRANS_CLIENT_KEY'),
+        'clientKey'     => env('MIDTRANS_CLIENT_KEY'),
         'isProduction'  => env('MIDTRANS_IS_PRODUCTION'),
         'isSanitized'   => env('MIDTRANS_IS_SANITIZED'),
         'is3ds'         => env('MIDTRANS_IS_3DS'),
